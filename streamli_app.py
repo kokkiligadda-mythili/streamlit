7b12c466d0d8f9b5dbcdd6d9f37ff4c0231257a1
kokkiligadda-mythili <140800690+kokkiligadda-mythili@users.noreply.github.com>
2024-03-31 19:14:59 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -1,3 +1,6 @@
-a = (1, 2, 3, 4, 5)
-x = sum(a)
-print(x)
+from keras.datasets import imdb
+
+# Load the data, keeping only 10,000 of the most frequently occuring words
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
+# Check the first review
+train_data[0]
